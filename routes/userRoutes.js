b4bb73913e05d1e3676bf5c99aa226a8e37b636f
Prathamesh Shashikant Chavan <117484138+Prathameshschavan@users.noreply.github.com>
2023-04-27 21:48:33 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,16 +15,5 @@ router.post("/register", registerValidatior, register);
 
 router.post("/login",loginValidatior, Login);
 
-// router.post("/verify",(req,res)=>{
-//     let token = req.body.token;
-//     try {
-//         let user = Verify(token);
-//         res.send("OK");
-//     } catch (error) {
-//         res.send("Not OK");
-//         console.log(error)
-//     }
-
-// })
 
 module.exports = router;
